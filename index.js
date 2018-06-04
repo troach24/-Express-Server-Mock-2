1bd29c5954ac8865c2541d0ac4805a0cc20d422a
Travis Roach <travis.roach24@gmail.com>
2018-06-04 08:42:31 -0600

    updated top level attribuite to be

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
-    instructors: data
-  })
+    data: data
+  });
 });
 
 app.get('/:id', (req, res) => {
@@ -17,23 +17,23 @@ app.get('/:id', (req, res) => {
   if(!id) {
     res.status(404).json({
       error: "Instructor record does not exist at this id."
-    })
+    });
   } else {
     res.json({
-      instructor: id
-    })
+      data: id
+    });
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
 
 function getInstructorById(data, id) {
   for(let i = 0; i < data.length; i++) {
     if(data[i].id == id) {
       return data[i]
     }
-  }
+  };
   return null;
 };
---
+++ rubric.md
@@ -0,0 +1,20 @@
+Passing requires all values
+
+## Functional
+
+* [] Deployed
+* [] Returns list
+* [] Returns single object at the top level
+* [] Uses a top-level `data` attribute
+* [] Attributes are camelCased
+* [] Data values are correct
+* [] Returns CORS headers
+* [] Returns 200 status codes for data
+* [] Returns 404 status codes for invalid IDs
+
+## Style
+
+* [] Correct indentation
+* [] Consistent semi-colon usage
+* [] Descriptive names
+* [] No `console.log` statements
